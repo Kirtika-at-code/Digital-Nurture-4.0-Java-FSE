af867129d66d282bfb255495c4dc8703cd7aa030
Kirtika-at-code <workforkirtika@gmail.com>
2025-07-04 21:42:51 +0530

    Update BookRepository.java

--- Week3_Spring Core and Maven/Exercise2_Implementing Dependency Injection/Code/repository/BookRepository.java
+++ Week3_Spring Core and Maven/Exercise2_Implementing Dependency Injection/Code/repository/BookRepository.java
@@ -1,12 +1,10 @@
 package com.library.repository;
 
 public class BookRepository {
-        public void display(){
-        System.out.println("BookRepository Bean is working");
-    }
-//    public void saveBook(String bookName) {
-//        System.out.println("Book saved: " + bookName);
-//    }
+        
+   public void saveBook(String bookName) {
+       System.out.println("Book saved: " + bookName);
+   }
 
 }
 
